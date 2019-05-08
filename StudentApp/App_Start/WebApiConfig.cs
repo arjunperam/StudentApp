3effb44d1fece36f0221ee7caf3543ab4b9276d0
CM2 ITSM <CM2 ITSM@DESKTOP-IAKH1PJ>
2019-05-08 17:44:13 +0530

    anothercommit

--- StudentApp/App_Start/WebApiConfig.cs
+++ StudentApp/App_Start/WebApiConfig.cs
@@ -19,6 +19,7 @@ namespace StudentApp
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.
         }
     }
 }
--- StudentApp/Controllers/StudentController.cs
+++ StudentApp/Controllers/StudentController.cs
@@ -4,10 +4,67 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using StudentDataAccess;
 
 namespace StudentApp.Controllers
 {
     public class StudentController : ApiController
     {
+        public IHttpActionResult Get()
+        {
+            using (StudentDbEntities entities = new StudentDbEntities())
+            {
+                return Ok(entities.StudentTables.ToList());
+            }
+        }
+        public IHttpActionResult Post(StudentTable student)
+        {
+            using (StudentDbEntities entities = new StudentDbEntities())
+            {
+                entities.StudentTables.Add(student);
+                entities.SaveChanges();
+                return Ok(entities.StudentTables.ToList());
+            }
+        }
+        public IHttpActionResult Put(int id, StudentTable student)
+        {
+            using (StudentDbEntities entities = new StudentDbEntities())
+            {
+                var entity = entities.StudentTables.FirstOrDefault(s => s.StudentId == id);
+                if (entity == null)
+                {
+                    return BadRequest("enter valid id");
+                }
+                else
+                {
+                    entity.StudentName = student.StudentName;
+                    entity.Course = student.Course;
+                    entity.Marks = student.Marks;
+                    entities.SaveChanges();
+                    return Ok(entities.StudentTables.ToList());
+                }
+            }
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            using (StudentDbEntities entities = new StudentDbEntities())
+            {
+                var entity = entities.StudentTables.FirstOrDefault(s => s.StudentId == id);
+
+                if (entity == null)
+                {
+                    return BadRequest("enter valid id");
+                }
+                else
+                {
+                    entities.StudentTables.Remove(entity);
+                    entities.SaveChanges();
+                    return Ok(entities.StudentTables.ToList());
+                }
+            }
+        }
     }
-}
+
+    }
+
